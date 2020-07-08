1175522324d2bfe4e812b026f9f2525a9dfdf3e0
ItanuRomero <66224956+ItanuRomero@users.noreply.github.com>
2020-07-08 12:25:05 -0300

    Publishing my program!

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # AlcoholOrGasoline
- If you have already thought about which one to buy, this app is for you.
+ If you have a flex-fuel car and already thought about which one to buy, this app is for you.
+Obs: I'm having another ideas to implement the project, do you want to help? Send me a message on LinkedIn!
+https://www.linkedin.com/in/itanu-romero-86968817b/
+See ya!
---
+++ whichOne?.py
@@ -0,0 +1,23 @@
+from os import system
+from time import sleep
+system('clear')
+print("Hey, I'm here to help you choose!!\n"
+      "Where you are...")
+sleep(3)
+gasoline = float(input('What is the price of gasoline? '))
+alcohol = float(input('And the price of alcohol? '))
+system('clear')
+if alcohol <= (gasoline * 0.7):
+    print('You have to choose alcohol! (etanol)\n\n')
+else:
+    print('You have to choose gasoline!!\n\n')
+sleep(5)
+print('Still having doubts?\n'
+      'Acess the link below:\n'
+      'https://www.aluguefoco.com.br/blog/post/alcool-ou-gasolina--como-saber-qual-combustivel-compensa-maisr\n'
+      '(Only in PT-BR, but you can translate it).')
+'''
+Program written by: Itanu Romero.
+For more information please contact me:
+https://www.linkedin.com/in/itanu-romero-86968817b/
+'''
